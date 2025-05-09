2da782881ebf8c1d8f0b62e0f219ac57cab30e16
이동욱 <wtpc99@naver.com>
2025-05-10 06:51:55 +0900

    feat:Dto

---
+++ src/main/java/com/FinGoal/api/user/dto/UserRequsetDto.java
@@ -0,0 +1,20 @@
+package com.FinGoal.api.user.dto;
+
+import com.FinGoal.api.user.domain.User;
+import lombok.Getter;
+
+@Getter
+public class UserRequsetDto {
+
+    private String id;
+    private String pw;
+    private String username;
+
+    public User toEntity(){
+        return User.builder()
+                .id(this.id)
+                .pw(this.pw)
+                .username(this.username)
+                .build();
+    }
+}
---
+++ src/main/java/com/FinGoal/api/user/dto/UserResponseDto.java
@@ -0,0 +1,18 @@
+package com.FinGoal.api.user.dto;
+
+import com.FinGoal.api.user.domain.User;
+import lombok.Builder;
+
+@Builder
+public class UserResponseDto {
+    private String id;
+    private String username;
+
+    public static UserResponseDto fromEntity(User user){
+        return UserResponseDto.builder()
+                .id(user.getId())
+                .username(user.getUsername())
+                .build();
+    }
+
+}
