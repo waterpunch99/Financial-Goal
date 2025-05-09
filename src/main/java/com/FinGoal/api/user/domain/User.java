7e2d4961097af47cc578194c816bc8f43e3f63c7
이동욱 <wtpc99@naver.com>
2025-05-10 05:52:08 +0900

    feat: User 엔티티 구현

--- src/main/java/com/FinGoal/api/user/domain/User.java
+++ src/main/java/com/FinGoal/api/user/domain/User.java
@@ -3,11 +3,14 @@ package com.FinGoal.api.user.domain;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 
-
+@NoArgsConstructor
 @Getter
 @Entity
 public class User {
@@ -15,7 +18,12 @@ public class User {
     @Id
     private String id;
     private String pw;
-    private String name;
-
+    private String username;
+    @Builder
+    public User(String id, String pw, String username){
+        this.id = id;
+        this.pw = pw;
+        this.username = username;
+    }
 
 }
